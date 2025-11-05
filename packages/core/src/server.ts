c22234a18c8e8b600d9acba5805858bf53796f44
Chris Tregaskis <49287297+ChrisTregaskis@users.noreply.github.com>
2025-11-05 23:02:39 +0000

    creation of tenant context

--- packages/core/src/server.ts
+++ packages/core/src/server.ts
@@ -4,11 +4,13 @@
  * This module exports Node.js-specific functionality that should ONLY be used
  * in server-side code (@ffp/functions, not @ffp/web):
  * - Database connections and utilities
+ *  - Tenant context extraction utilities
  * - Error classes for API responses
  * - Lambda function wrappers
  * - AWS Cognito service utilities
+
  *
- * Import using: import { db, withRLS, ValidationError, withErrorHandling, cognito } from '@ffp/core/server';
+ * Import using: import { db, withRLS, ValidationError, withErrorHandling, cognito, extractUserContext } from '@ffp/core/server';
  */
 
 export * from './lib/database';
