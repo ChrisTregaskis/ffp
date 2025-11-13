7ba882d75b847770678dcad35e5b3ff7349f5485
Chris Tregaskis <49287297+ChrisTregaskis@users.noreply.github.com>
2025-11-13 14:10:48 +0000

    slight refactor to tidy code

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -3,11 +3,10 @@ import { IconTest } from '@web/pages/IconTest';
 import { FormTest } from './pages/FormTest';
 
 function App(): JSX.Element {
-  // Test for showing IconTest page for FFP-115c verification
   return (
     <>
-      <IconTest />;
-      <FormTest />;
+      <IconTest />
+      <FormTest />
     </>
   );
 }
